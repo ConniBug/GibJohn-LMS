38b806eeb6942e1054ae73a40b042c9302c8ab74
Conni <connithekiwi@gmail.com>
2022-05-24 13:21:22 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -5,7 +5,7 @@ define("WEBROOT_API", WEBROOT . "api");
 define("WEBROOT_FRONTEND", WEBROOT . "frontend");
 
 // MySQL Database connection credentials.
-define("MYSQL_HOST", "localhost");
-define("MYSQL_USERNAME", "GibJohn_Web");
-define("MYSQL_PASSWORD", "DFSiobfs");
+define("MYSQL_HOST", "<REPLACE ME>");
+define("MYSQL_USERNAME", "<REPLACE ME>");
+define("MYSQL_PASSWORD", "<REPLACE ME>");
 define("MYSQL_DATABASE_NAME", "gibjohn_web");
